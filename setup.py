35a016aa26712c1548bcdec5e7f8b28d0a6bc57b
LaFa777 <belogurov.kirill@gmail.com>
2018-09-30 01:27:57 +0500

    Оформил как пакет

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='telegram_addons',
+      version='0.1.0',
+      description='Extensions for python-telegram-bots',
+      author='Kryglyash',
+      packages=find_packages(),
+      )
