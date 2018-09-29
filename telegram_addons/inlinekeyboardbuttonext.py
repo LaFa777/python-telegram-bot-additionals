75b2de2f9bb2e8230cb1b8cb8f36566967a5276b
LaFa777 <belogurov.kirill@gmail.com>
2018-09-30 01:25:04 +0500

    Добавлен InlineKeyboardButtonExt

---
+++ telegram_addons/inlinekeyboardbuttonext.py
@@ -0,0 +1,18 @@
+from telegram import InlineKeyboardButton
+
+
+class InlineKeyboardButtonExt(InlineKeyboardButton):
+    """Добавляет возможность более точно указать обработчика используя
+    `telegram.CallbackQueryHandlerExt` путем указания параметра command.
+    Параметр command используется для последующего преобразования ``callback_data`` в ``BotProxy``.
+    """
+
+    def __init__(self, text, command, *args, **kwargs):
+        self.command = command
+        super().__init__(text, *args, **kwargs)
+
+    def to_dict(self):
+        # command не является параметром для api телеграма
+        if "command" in self.__dict__:
+            del self.command
+        return super().to_dict()
